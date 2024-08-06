4f395dfeb75e488eeb9010bf80e216b10d790834
Ryebuck Junction / Rai / SpaceFunk <90148699+SpaceFunkRai@users.noreply.github.com>
2024-08-06 19:17:57 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿public class Program
+{
+    public static int mainRadix = 60;
+    public static int subMinorRadix = 6;
+    public static int subMajorRadix = 10;
+
+    public static void Main()
+    {
+        string NM = Console.IsOutputRedirected ? "" : "\x1b[97m";
+        string GR = Console.IsOutputRedirected ? "" : "\x1b[92m";
+        string YE = Console.IsOutputRedirected ? "" : "\x1b[93m";
+
+        Console.WriteLine("———Decimal To MixedRadixBijective———");
+        Console.WriteLine($"{NM}Enter an {GR}integer{NM} (or type 'exit' to quit): ");
+        
+        while (true)
+        {
+            string? input = Console.ReadLine();
+
+            if (input?.ToLower() == "exit")
+                break;
+            
+
+            if (int.TryParse(input, out int number))
+                Console.WriteLine($"Decimal {GR}{number}{NM} is {GR}{GetSubBase(ParseBase(ConvertToBase(number, mainRadix)))}{NM} in base {YE}{mainRadix}{NM} bijective with {YE}[{subMajorRadix}, {subMinorRadix}]{NM} sub-radices.");
+            
+            else
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            
+        }
+    }
+
+    private static string GetSubBase(int[] subBases)
+    {
+        string sub = "";
+
+        foreach (int subBase in subBases)
+        {
+            string s = "";
+
+            for (int i = 0; i < ParseBase(ConvertToBase(subBase, subMinorRadix, subMajorRadix)).Length; i++)
+            {
+                if (i % 2 == 0)
+                    s += ParseBase(ConvertToBase(subBase, subMinorRadix, subMajorRadix))[i];
+
+                else if (i % 2 == 1)
+                    s += Units(ParseBase(ConvertToBase(subBase, subMinorRadix, subMajorRadix))[i]);
+            }
+
+            bool b = int.TryParse(s, out int n);
+
+            if (n <= 6 && b)
+                s = Units(n);
+
+            sub += s;
+        }
+
+        return sub;
+    }
+
+    static string Units(int number) => number switch
+    {
+        0 => "0",
+        1 => "A",
+        2 => "B",
+        3 => "C",
+        4 => "D",
+        5 => "E",
+        6 => "F",
+        _ => "",
+    };
+
+    static string ConvertToBase (int dec, int radix, int subRadix = -1, bool bijective = true)
+    {
+        if (dec == 0)
+            return "0";
+
+        int carryRadix = subRadix == -1 ? radix : subRadix;
+        
+        int quotient  = dec / radix;
+        int remainder = dec % radix;
+
+        if (bijective && remainder == 0)
+        {
+            remainder = radix;
+            quotient--;
+        }
+
+        if (quotient == 0)
+            return $"{remainder}";
+        
+        else if (quotient > radix)
+            return $"{ConvertToBase(quotient, carryRadix)}:{remainder}";
+        
+        else
+            return $"{quotient}:{remainder}";
+    }
+
+    static int[] ParseBase(string s) => s.Split(':').Select(int.Parse).ToArray();
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Decimal-To-Bijective-MixedRadix-Base
+A small C# script I originally made to convert decimal integers to a spicy base 60 numerical system I made for my world-building, extended to allow for more bases ;3
+
+The code was originally built to run on the command-line, but it should be quick and easy to repurpose for basically anything ;p
+
+~~I don't even know if anyone will ever need this lol~~
